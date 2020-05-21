d4c60cd35032778da17da1ea418c6907cc5041af
Volodymyr Karabchhuk <63458060+volodymyr-karabchuk@users.noreply.github.com>
2020-05-21 15:50:48 +0300

    Update run.sh
    тут уже как кому нравится но думаю так будет правильнее

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ cd ansible
 if [ "$1" == "app2" ]
 then
     ansible-playbook -i hosts app2.yml
-else
+elif [ "$1" == "app1" ]
+then
     ansible-playbook -i hosts app1.yml
+else 
+    echo "bad sytax"
 fi
